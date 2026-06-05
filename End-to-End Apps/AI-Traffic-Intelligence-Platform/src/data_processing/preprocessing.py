deaaf19a3c5cf1d6f9a1f5c68937ef9ff3a287fe
MohammedAyanSyed <ayansyed191919@gmail.com>
2026-06-05 15:25:24 +0530

    reusable

--- End-to-End Apps/AI-Traffic-Intelligence-Platform/src/data_processing/preprocessing.py
+++ End-to-End Apps/AI-Traffic-Intelligence-Platform/src/data_processing/preprocessing.py
@@ -1,44 +1,40 @@
-import numpy as np
+# Import Libraries
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
+# CSV File
 df = pd.read_csv('https://raw.githubusercontent.com/MohammedAyanSyed/projects/refs/heads/main/End-to-End%20Apps/'
                  'AI-Traffic-Intelligence-Platform/data/processed/traffic_fe.csv',parse_dates=['time'])
 df_selected = pd.read_csv('https://raw.githubusercontent.com/MohammedAyanSyed/projects/refs/heads/main/End-to-End%20Apps/'
                           'AI-Traffic-Intelligence-Platform/data/processed/traffic_selected.csv')
-
-# All Feature Preprocessing
-# Splitting
-independent = df.drop(['congestion_level','region_index','time'],axis=1)
+# Splitting CSV All Features :
+independent = df.drop(['congestion_level','region_label','time'],axis=1)
 dependent = df['congestion_level']
-Xtrain_fe,Xtest_fe,ytrain_fe,ytest_fe = train_test_split(independent,dependent,train_size=0.8,random_state=33)
-cat_cols = Xtrain_fe.select_dtypes(include=['object','category']).columns.to_list()
-num_cols = Xtrain_fe.select_dtypes(include=['int','float']).columns.to_list()
 
-# Encoding
-for col in cat_cols:
-    freq = Xtrain_fe[col].value_counts(normalize=True)
-    Xtrain_fe[col] = Xtrain_fe[col].map(freq)
-    Xtest_fe[col] = Xtest_fe[col].map(freq)
+# Splitting CSV Selected Features :
+independent_2 = df.drop('congestion_level',axis=1)
+dependent_2 = df['congestion_level']
 
-# Scaling
-SS = StandardScaler()
-Xtrain_fe[num_cols] = SS.fit_transform(Xtrain_fe[num_cols])
-Xtest_fe[num_cols] = SS.transform(Xtest_fe[num_cols])
+# Preprocessing
+def preprocessed(x,y):
+    Xtrain,Xtest,ytrain,ytest = train_test_split(x,y,train_size=0.9,random_state=33)
+    num_cols = Xtrain.select_dtypes(include=['int','float']).columns.to_list()
+    cat_cols = Xtrain.select_dtypes(include=['object','category']).columns.to_list()
 
-# Selected Feature Preprocessing
-# Splitting
-independent_selected = df_selected.drop('congestion_level',axis=1)
-dependent_selected = df_selected['congestion_level']
-Xtrain_select,Xtest_select,ytrain_select,ytest_select = train_test_split(independent_selected,dependent_selected,train_size=0.9,random_state=33)
-cat_cols2 = Xtrain_select.select_dtypes(include=['category','object']).columns.to_list()
-num_cols2 = Xtrain_select.select_dtypes(include=['int','float']).columns.to_list()
-# Encoding
-for col in cat_cols2:
-    freq = Xtrain_select[col].value_counts(normalize=True)
-    Xtrain_select[col] = Xtrain_select[col].map(freq)
-    Xtest_select[col] = Xtest_select[col].map(freq)
-# Scaling
-Xtrain_select[num_cols2] = SS.fit_transform(Xtrain_select[num_cols2])
-Xtest_select[num_cols2] = SS.transform(Xtest_select[num_cols2])
+    # Encoding
+    for col in cat_cols:
+        freq = Xtrain[col].value_counts(normalize=True)
+        Xtrain[col] = Xtrain[col].map(freq)
+        Xtest[col] = Xtest[col].map(freq)
+
+    # Scaling
+    SS = StandardScaler()
+    Xtrain[num_cols] = SS.fit_transform(Xtrain[num_cols])
+    Xtest[num_cols] = SS.transform(Xtest[num_cols])
+
+    return Xtrain,Xtest,ytrain,ytest
+
+# Preprocessed Splits
+Xtrain_fe,Xtest_fe,ytrain_fe,ytest_fe = preprocessed(independent,dependent)
+Xtrain_select,Xtest_select,ytrain_select,ytest_select = preprocessed(independent_2,dependent_2)
